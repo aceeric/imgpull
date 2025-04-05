d4c7ca0db4c838f1ce691bcb0967a7b7c1d04821
Eric Ace <24485843+aceeric@users.noreply.github.com>
2025-04-05 17:48:14 -0400

    Fix incorrect case in json annotation

--- pkg/imgpull/manifest_holder.go
+++ pkg/imgpull/manifest_holder.go
@@ -61,7 +61,7 @@ var manifestTypeToString = map[ManifestType]string{
 
 // ManifestHolder holds one of: v1 oci manifest list, v1 oci manifest, docker v2
 // manifest list, or docker v2 manifest. The original data from the upstream
-// is also in the 'Data' member of the struct. The 'Bytes' member is the authoritative
+// is also in the 'Bytes' member of the struct. The 'Bytes' member is the authoritative
 // representation of the upstream data: if you compute a digest from it, the digest
 // will match the 'Digest' field (also from the upstream) The other fields like
 // 'V1ociIndex' are cosmetic and may not produce the same digest as the 'Bytes'
@@ -74,7 +74,7 @@ type ManifestHolder struct {
 	V1ociIndex           v1oci.Index           `json:"v1.oci.index"`
 	V1ociManifest        v1oci.Manifest        `json:"v1.oci.manifest"`
 	V2dockerManifestList v2docker.ManifestList `json:"v2.docker.manifestList"`
-	V2dockerManifest     v2docker.Manifest     `json:"v2.docker.Manifest"`
+	V2dockerManifest     v2docker.Manifest     `json:"v2.docker.manifest"`
 }
 
 // ToString renders the manifest held by the receiver into JSON. Only the
